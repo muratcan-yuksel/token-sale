5152d94e7f8eca8cb90031351554e9de0afd9e65
muratcan-yuksel <muratcanyukselpro@gmail.com>
2024-05-11 22:11:44 +0700

    contract is complete, needs to be tested

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -17,7 +17,9 @@ contract TokenSale is Ownable, ReentrancyGuard {
     mapping(address => uint256) public balances;
     mapping(address => bool) public whitelistedAddresses;
 
-    bool public saleActive = true;
+    bool public saleActive = false;
+    bool public whitelistSaleActive = true;
+    bool public paused = false;
     //events
     event TokenSold(address indexed buyer, uint256 amount, uint256 valueInUSDT);
 
@@ -29,8 +31,135 @@ contract TokenSale is Ownable, ReentrancyGuard {
         minSale = 1;
     }
 
+    //view functions
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        return whitelistedAddresses[_address];
+    }
+
+    function isSaleActive() public view returns (bool) {
+        return saleActive;
+    }
+
+    function isWhitelistSaleActive() public view returns (bool) {
+        return whitelistSaleActive;
+    }
+
+    function getTokenPrice() public view returns (uint256) {
+        return tokenPrice;
+    }
+
+    function getTokenSold() public view returns (uint256) {
+        return tokenSold;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getMaxSale() public view returns (uint8) {
+        return maxSale;
+    }
+
+    function getMinSale() public view returns (uint8) {
+        return minSale;
+    }
+
+    function getPaused() public view returns (bool) {
+        return paused;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner();
+    }
+
+    //write functions
     function setSaleLimits(uint8 _maxSale, uint8 _minSale) external onlyOwner {
         maxSale = _maxSale;
         minSale = _minSale;
+    }
+
+    function toggleWhitelistSaleActive() external onlyOwner {
+        whitelistSaleActive = !whitelistSaleActive;
+    }
+
+    function toggleSaleActive() external onlyOwner {
+        saleActive = !saleActive;
+    }
+
+    function addToWhitelist(address _address) external onlyOwner {
+        whitelistedAddresses[_address] = true;
+        // emit AddedToWhitelist(_address);
+    }
+
+    function removeFromWhitelist(address _address) external onlyOwner {
+        delete whitelistedAddresses[_address];
+        // emit RemovedFromWhitelist(_address);
+        // or we can do
+        //whitelistedAddresses[_address] = false;
+    }
+
+    function pause() external onlyOwner {
+        paused = true;
+    }
+
+    function unpause() external onlyOwner {
+        paused = false;
+    }
+
+    function setTokenPrice(uint256 _tokenPrice) external onlyOwner {
+        tokenPrice = _tokenPrice;
+    }
+
+    function buyWhitesaleTokens(uint256 _amount) external payable {
+        require(whitelistSaleActive, "WhiteSale is not active");
+        require(paused == false, "Contract is paused");
+        require(
+            saleActive == false,
+            "Standard should be inactive during the whitesale"
+        );
+        require(whitelistedAddresses[msg.sender], "You are not whitelisted");
+        require(
+            _amount >= minSale && _amount <= maxSale,
+            "Amount must be between 1 and 3"
+        );
+        require(
+            msg.value >= _amount * tokenPrice,
+            "You have to pay the correct amount"
+        );
+
+        awtoken.transfer(msg.sender, _amount);
+        tokenSold += _amount;
+        //emit TokenSold(msg.sender, _amount, _amount * tokenPrice);
+    }
+
+    function buyTokens(uint256 _amount) external payable {
+        require(saleActive, "Standard sale is not active");
+        require(paused == false, "Contract is paused");
+        require(whitelistSaleActive == false, "WhiteSale should not be active");
+        require(
+            _amount >= minSale && _amount <= maxSale,
+            "Amount must be between 1 and 3"
+        );
+        require(
+            msg.value >= _amount * tokenPrice,
+            "You have to pay the correct amount"
+        );
+    }
+
+    function withdrawEth() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function withdrawToken() external onlyOwner {
+        awtoken.transfer(msg.sender, address(this).balance);
+    }
+
+    fallback() external payable {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    receive() external payable {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
